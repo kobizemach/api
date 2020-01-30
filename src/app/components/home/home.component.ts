86b99044fa5631b39ec35c4d6f6469e1de7f0140
kobi zemach <kobi@aplicatzia.com>
2020-01-30 12:59:06 +0200

    upgrade my app

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
-import { map } from 'rxjs/operators';
+//import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -8,15 +8,24 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  bbb:any;
+
   people:any;
 
   constructor(private api:ApiService) { }
 
   ngOnInit() {
 
-    this.api.handleSearch()
+    //this.bbb();
 
+  }
+
+  bbb(){
+    this.api.getData().subscribe(res => 
+      {
+        this.people = res;
+        console.log(this.people);
+      }
+    );
   }
 
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import fetchJsonp from 'fetch-jsonp'; 
 
 
 @Injectable({
@@ -11,27 +10,24 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   getData() {
-    const url = 'https://storage.googleapis.com/static.aoni.io/demo/user.json?callback=JSONP_CALLBACK';
-    this.http.jsonp(url, 'callback')
-    .pipe(
-      catchError(this.handleError('jsonpTest', 'ERROR')),
-    ).subscribe(data => {
-      this.log('data: ' + JSON.stringify(data));
-    });
+    const url = '/static.aoni.io/demo/user.json';
+    
+    return this.http.get(url)
 
+    //return this.http.jsonp(url, 'callback')
 
-    // fetchJsonp(url)
-    // .then(function(response) {
-    //   return response.json()
-    // }).then(function(json) {
-    //   console.log('parsed json', json)
-    // }).catch(function(ex) {
-    //   console.log('parsing failed', ex)
-    // })
   }
 
-  handleSearch(){
-    let url = 'https://storage.googleapis.com/static.aoni.io/demo/user.json';
-    
+
+
+
+
+
+
+
+
+
+
+
 
 }
